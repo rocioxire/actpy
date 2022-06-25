be979b42970c90d924a9c1385c8f938fbfec083e
rocioxire <98195035+rocioxire@users.noreply.github.com>
2022-06-24 23:02:50 -0500

    Create act1_sumaenteros.py
    Función que suma dos enteros.
    no valida que los números sean enteros, así que devuelve error al meter caracteres diferentes a enteros.

---
+++ act1_sumaenteros.py
@@ -0,0 +1,12 @@
+#Crea una función que sume dos números enteros
+
+x = input("Primer número entero: ")
+y = input("Segundo número entero: ")
+x = int(x)
+y = int(y)
+
+def SumaEnteros(x,y):
+    s = x + y
+    print(s)
+
+SumaEnteros(x,y)
